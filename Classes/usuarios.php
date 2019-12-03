d7f1d42f1bc0a4b5c121ff79089bcba70a97946c
WellingtonWDS <44651361+WellingtonWDS@users.noreply.github.com>
2019-12-03 17:40:19 -0300

    Update usuarios.php
    CRUD

--- Classes/usuarios.php
+++ Classes/usuarios.php
@@ -64,16 +64,128 @@ Class Usuario{
             echo "Erro";
         }
     }
+    public function alterarNome($nome, $id){
+        global $pdo;
+        $sql= $pdo->prepare("UPDATE usuario SET NOME = :n WHERE IDUSUARIO = :id");
+        $sql->bindValue(":n",$nome);
+        $sql->bindValue(":id",$id);
+        $sql->execute();
 
-    public function getId(){
-        return $this->id;
+        if($sql->rowCount() == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
     }
 
+    public function alterarData($data, $id){
+        global $pdo;
+        $sql= $pdo->prepare("UPDATE usuario SET DATANASC = :d WHERE IDUSUARIO = :id");
+        $sql->bindValue(":d",$data);
+        $sql->bindValue(":id",$id);
+        $sql->execute();
 
+        if($sql->rowCount() == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
 
-   
+    public function alterarEmail($email, $id){
+        global $pdo;
+        // Verificar se o email já está cadastrado
+        $sql = $pdo->prepare("SELECT IDUSUARIO FROM usuario WHERE EMAIL = :e");
+        $sql->bindValue(":e", $email);
+        $sql->execute();
+        
 
+        //Se está, não cadastra e retorna false
+        if($sql->rowCount() > 0){
+            $msgErro = "E-mail já cadastrado!";
+            return false;
+        }
+        else{
+            $sql= $pdo->prepare("UPDATE usuario SET EMAIL = :e WHERE IDUSUARIO = :id");
+            $sql->bindValue(":e",$email);
+            $sql->bindValue(":id",$id);
+            $sql->execute();
+
+            if($sql->rowCount() == 1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        
+    }
+
+    public function alterarDDD($ddd, $id){
+        global $pdo;
+        $sql= $pdo->prepare("UPDATE usuario SET DDD = :d WHERE IDUSUARIO = :id");
+        $sql->bindValue(":d",$ddd);
+        $sql->bindValue(":id",$id);
+        $sql->execute();
+
+        if($sql->rowCount() == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+
+    public function alterarTelefone($tel, $id){
+        global $pdo;
+        $sql= $pdo->prepare("UPDATE usuario SET TELEFONE = :t WHERE IDUSUARIO = :id");
+        $sql->bindValue(":t",$tel);
+        $sql->bindValue(":id",$id);
+        $sql->execute();
+
+        if($sql->rowCount() == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+
+    public function alterarSenha($senha, $id){
+        global $pdo;
+        $sql= $pdo->prepare("UPDATE usuario SET SENHA = :s WHERE IDUSUARIO = :id");
+        $sql->bindValue(":s",md5($senha));
+        $sql->bindValue(":id",$id);
+        $sql->execute();
+
+        if($sql->rowCount() == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function comparaSenha($senha, $id){
+        global $pdo;
+        $sql = $pdo->prepare("SELECT IDUSUARIO FROM usuario WHERE SENHA = :s and IDUSUARIO = :id");
+        $sql->bindValue(":s",md5($senha));
+        $sql->bindValue(":id",$id);
+        $sql->execute();
+
+        if($sql->rowCount() > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
-
-
 ?>
