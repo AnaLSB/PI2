8869ceb2c330c70cc00f9e0acb1ce35c92c8e1a8
WellingtonWDS <44651361+WellingtonWDS@users.noreply.github.com>
2019-12-09 02:22:20 -0300

    Estilization

--- src/my-routes/my-routes.php
+++ src/my-routes/my-routes.php
@@ -21,11 +21,7 @@ if (isset($_GET['from'])){
     
     $from = $_GET['from'];
     $to = $_GET['to'];
-
-    
-
 }
-
 
 ?>
 
@@ -36,11 +32,27 @@ if (isset($_GET['from'])){
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link  href="../../node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
     <link  href="../../node_modules/glyphicons-only-bootstrap/css/bootstrap.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link href="./my-routes.css" rel="stylesheet" />
    
     <title>Caronas</title>
 
     <style>
+        body{
+             font-family: 'Roboto', sans-serif;
+             font-size: 16px;
+        }
+        .bg{
+          position: fixed;
+          top: 0px;
+          left: 0px;
+          width: 100%;
+          height: 100%;
+          background-image: url(./bg.png);
+          background-size: 100%;
+          background-repeat: no-repeat;
+        }
+
 
         h3{
             margin-top: 100px;
@@ -53,7 +65,7 @@ if (isset($_GET['from'])){
             height: 35px;
             font-size: 25px;
             color: #FFF !important;
-            background: #64C9CC;
+            background: #336b77;
             border-radius: 50%;
         }
 
@@ -74,9 +86,49 @@ if (isset($_GET['from'])){
             }
         }
 
+        .price {
+          position: absolute;
+          font-weight: bold;
+          color: rgb(1, 248, 112) !important;
+          font-size: 18px;
+          left: 0px;
+          width: 100px;
+        }
+        .btn-price{
+          background-color: rgb(1, 248, 112);
+          color: #FFF;
+          border-radius: 18px 0px 0px 0px;
+          font-weight: bold;
+        }
+        .container-ride {
+          margin: 20px auto;
+          position: relative;
+          border-radius: 18px;
+          width: cover;
+          height: 220px;
+          -webkit-box-shadow: 0px 3px 5px 0px rgba(184,175,184,1);
+          -moz-box-shadow: 0px 3px 5px 0px rgba(184,175,184,1);
+          box-shadow: 0px 3px 5px 0px rgba(184,175,184,1);
+          min-width: 100%;
+          max-width: 700%;
+        }
+        .arrow {
+          margin-top: 20px;
+          transform: rotate(-45deg);
+          -webkit-transform: rotate(-45deg);
+        }
+        .sair{
+            width: 250px;
+            height: 50px;
+            padding-top: 15px;
+        }
+        .sair:hover{
+            background-color: #104b77
+        }
     </style>
 </head>
-<body style="background: #F9F9F9;">
+<body>
+    <div class="bg"></div>
     
         <div id="wrapper">
                 <!-- Sidebar -->
@@ -88,6 +140,9 @@ if (isset($_GET['from'])){
                 <li><a href="../offer-lift/offer-lift.php">Oferecer</a></li>
                 <li><a href="../my-routes/my-routes.php">Rotas</a></li>
                 <li><a href="../profile/profile.php">Meus Dados</a></li>
+                <a style=" color: white; position: absolute; bottom: 0px;left: 0px;" href="../../sair.php">
+                    <div class="sair"><h5>Sair</h5></div>
+                </a>
                 </ul>
             </div>
             
@@ -102,74 +157,44 @@ if (isset($_GET['from'])){
 
                     <div class="container">
                         
-                    <h3>Minhas Rotas</h3>
+                    <h3 align="center" style="color: #336b77; margin: 50px 0px auto;">Minhas Rotas</h3>
                         
 
                         <?php 
-                        
-                    
-                    
-
                                 foreach($service->querySelect($id) as $value ) { 
                             
                             ?>
-
-                   
-                        
-                            <div class="container-ride" style="background: #FFF;">
+                            <div class="container-ride" style="background: #FFF;color: #84afb9">
 
                                 
                                     <a href="../ride-pending/ride-pending.php?id=<?=$value['IDCARONA']?>"><p class="NumSolicit right"><?=$reqService->querySelectNumRows($value['IDCARONA']);?></p></a>
 
                                     <a href="../update-ride/update-ride.php?id=<?=$value['IDCARONA']?>">
-                                        <p class="price right">R$ <?= $value['PRECO'] ?></p>
-
                                         
-                                        <p><?= $value['ORIGEM'] ?> &nbsp; <i class="arrow"></i>
+
+                                        <!--#84afb9-->
+                                        <button class="btn btn-price">R$ <?= $value['PRECO'] ?></button>
+                                        <p style="margin: 0px auto; color: #84afb9; font-size: 20px"><?= $value['ORIGEM'] ?> &nbsp; 
+                                        <i class="arrow"></i>
                                         <i class="arrow"></i>
                                         <i class="arrow"></i>       
                                         &nbsp; <?= $value['DESTINO'] ?> </p>
 
 
-                                        <p><?= $time = $format->formatTime($value['HORARIO'])?> &nbsp;
+                                        <p style="margin: 2% auto;color: #84afb9; font-size: 20px;"><?= $time = $format->formatTime($value['HORARIO'])?> &nbsp;
 
                                             <span class="glyphicon glyphicon-time"></span>
 
                                         &nbsp; <?= $timetwo = $format->formatTime($value['HORAVOLTA'])?></p>
 
-
-                                        
-                                    
-                                        
-                                
-                                        <p class="date" style="position: absolute; bottom: -10px; left: 22%;" >                             
+                                        <p class="date" style="margin: 2% auto; color: #84afb9; font-size: 20px;">                             
                                             <?= $data = $format->formatDate($value['DATA']); ?>
                                         </p>
                                    </a>
-
-                              
                             </div>
- 
-
                         <?php
-
                                 }
-                    
-                            
                         ?>
-
-                       
-
-
-                        
-
-
-                            
-                        
-
-
-                        
-                        
                         </div>
                     </div>
                 </div>
